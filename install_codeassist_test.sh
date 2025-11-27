2b972760821c32f6bc043efa37b1f9bb9bbc9854
jetrx32 <jetrx32@gmail.com>
2025-11-27 15:20:45 +0300

    Update install_codeassist_test.sh

--- install_codeassist_test.sh
+++ install_codeassist_test.sh
@@ -4,7 +4,10 @@ HFTOKEN=$1
 git clone https://github.com/gensyn-ai/codeassist
 cd codeassist
 wget https://raw.githubusercontent.com/jetrx32/codeassist_inst/refs/heads/main/tmux_controller_api.py
-
+wget https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64
+chmod +x cloudflared-linux-amd64
 apt install python3-pip -y
-tmux new-session -d -s codeassist 'export HF_TOKEN=$HFTOKEN && uv run run.py'
-tmux new-session -d -s codeassist 'python3 tmux_controller_api.py'
+nohup ./cloudflared-linux-amd64 tunnel --url http://localhost:3000 > "/root/codeassist/cloudflared.log" 2>&1 &
+
+tmux new-session -d -s codeassist "export HF_TOKEN='$HFTOKEN' && uv run run.py; bash"
+tmux new-session -d -s control_api 'python3 tmux_controller_api.py; bash'
