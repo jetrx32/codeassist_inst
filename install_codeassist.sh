8b3ecee4bdb9af889949dc1b3ecf38af64b28770
jetrx32 <jetrx32@gmail.com>
2025-11-27 17:05:56 +0300

    Update and rename install_codeassist_test.sh to install_codeassist.sh

--- install_codeassist.sh
+++ install_codeassist.sh
@@ -1,9 +1,16 @@
 
 HFTOKEN=$1
 
+# Остановка и удлаение
+docker stop codeassist-zero-style-ui codeassist-solution-tester codeassist-state-service codeassist-web-ui codeassist-policy-model codeassist-ollama
+docker rm codeassist-zero-style-ui codeassist-solution-tester codeassist-state-service codeassist-web-ui codeassist-policy-model codeassist-ollama
+rm -rf codeassist
+
+# Копирование репы codeassist
 git clone https://github.com/gensyn-ai/codeassist
 cd codeassist
 
+# Установка вспомогоательных скриптов и инструементов
 wget https://raw.githubusercontent.com/jetrx32/codeassist_inst/refs/heads/main/tmux_controller_api.py
 wget https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64
 wget https://raw.githubusercontent.com/jetrx32/codeassist_inst/refs/heads/main/fastapi_clf.py
@@ -12,9 +19,11 @@ chmod +x cloudflared-linux-amd64
 apt install python3-pip -y
 python3 -m pip install uvicorn flask fastapi
 
+
+# Запуск тунела и апи для пересылки на тунел по ip port
 nohup ./cloudflared-linux-amd64 tunnel --url http://localhost:3000 > "/root/codeassist/cloudflared.log" 2>&1 &
 nohup python3 fastapi_clf.py  > "/root/codeassist/fastapi_cloudflared.log" 2>&1 &
 
-
+# Запуск codeassist и api для управления
 tmux new-session -d -s codeassist "export HF_TOKEN='$HFTOKEN' && uv run run.py; bash"
 tmux new-session -d -s control_api 'python3 tmux_controller_api.py; bash'
