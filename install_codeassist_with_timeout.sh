c4e19d1892a2be022acf6b6073e5b0b9292ce58b
jetrx32 <jetrx32@gmail.com>
2025-12-03 19:03:50 +0300

    Update install_codeassist_with_timeout.sh

--- install_codeassist_with_timeout.sh
+++ install_codeassist_with_timeout.sh
@@ -31,16 +31,14 @@ cd web-ui
 cd ..
 # Установка вспомогоательных скриптов и инструементов
 wget -O tmux_controller_api.py https://raw.githubusercontent.com/jetrx32/codeassist_inst/refs/heads/main/tmux_newcontroller.py
-wget https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64
 wget https://raw.githubusercontent.com/jetrx32/codeassist_inst/refs/heads/main/fastapi_clf.py
 
-chmod +x cloudflared-linux-amd64
 apt install python3-pip -y
 python3 -m pip install uvicorn flask fastapi psutil
 
 
 # Запуск тунела и апи для пересылки на тунел по ip port
-nohup ./cloudflared-linux-amd64 tunnel --url http://localhost:4004 > "/root/codeassist/cloudflared.log" 2>&1 &
+nohup /root/cloudflared/cloudflared tunnel --url http://localhost:4004 > "/root/codeassist/cloudflared.log" 2>&1 &
 nohup python3 fastapi_clf.py  > "/root/codeassist/fastapi_cloudflared.log" 2>&1 &
 
 # Запуск codeassist и api для управления
